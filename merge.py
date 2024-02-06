d18f748ec62d03110e9046cba1dbb8e238dd1bf9
Gabriel Couto <gabrielcouto3.14@gmail.com>
2024-02-06 16:02:22 -0300

    feat: added alphabetical sorting to elements

--- merge.py
+++ merge.py
@@ -1,31 +1,32 @@
 import json
 
-def merge_jsons(json1, json2):
+def merge_and_sort_jsons(json1, json2):
     merged_json = {"discoveries": [], "elements": []}
 
-    # Merge elements from the first JSON
+    # Merge elements without duplicates
     for element in json1["elements"]:
         if element not in merged_json["elements"]:
             merged_json["elements"].append(element)
 
-    # Merge elements from the second JSON
     for element in json2["elements"]:
         if element not in merged_json["elements"]:
             merged_json["elements"].append(element)
 
+    # Sort elements by the "text" field in ascending order
+    # Comment next line if you dont want to rearrange elements in alphabetical order
+    merged_json["elements"] = sorted(merged_json["elements"], key=lambda x: x.get("text", ""))
+
     return merged_json
 
-# Ler os JSONs dos arquivos
 with open('json1.json', 'r') as file1, open('json2.json', 'r') as file2:
     json1 = json.load(file1)
     json2 = json.load(file2)
 
-# Mesclar os JSONs
-merged_json = merge_jsons(json1, json2)
+# Merge and sort the JSONs
+merged_json = merge_and_sort_jsons(json1, json2)
 
-# Escrever o JSON mesclado em um novo arquivo
 with open('import.js', 'w') as output_file:
-    js_code =(
+    js_code = (
         f'a = {json.dumps(merged_json, indent=2)};\n\n'
         'window.$nuxt.$root.$children[2].$children[0].$children[0]._data.elements = a.elements;\n'
         'window.$nuxt.$root.$children[2].$children[0].$children[0]._data.discoveries = a.discoveries;\n'
