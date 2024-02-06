6de0ad501550e6c40d72a2ef69bcdab8e21b83ac
Gabriel Couto <gabrielcouto3.14@gmail.com>
2024-02-06 15:23:47 -0300

    fix: fixed some logical bugs, now working optimally

--- merge.py
+++ merge.py
@@ -1,35 +1,36 @@
 import json
 
-def generate_js_code(file1, file2):
-    with open(file1, 'r') as f1, open(file2, 'r') as f2:
-        json1 = json.load(f1)
-        json2 = json.load(f2)
-
+def merge_jsons(json1, json2):
     merged_json = {"discoveries": [], "elements": []}
 
-    for element1, element2 in zip(json1["elements"], json2["elements"]):
-        if element1 != element2:
-            merged_json["elements"].append(element1)
-            merged_json["elements"].append(element2)
-        else:
-            merged_json["elements"].append(element1)
+    # Merge elements from the first JSON
+    for element in json1["elements"]:
+        if element not in merged_json["elements"]:
+            merged_json["elements"].append(element)
 
-    js_code = (
+    # Merge elements from the second JSON
+    for element in json2["elements"]:
+        if element not in merged_json["elements"]:
+            merged_json["elements"].append(element)
+
+    return merged_json
+
+# Ler os JSONs dos arquivos
+with open('json1.json', 'r') as file1, open('json2.json', 'r') as file2:
+    json1 = json.load(file1)
+    json2 = json.load(file2)
+
+# Mesclar os JSONs
+merged_json = merge_jsons(json1, json2)
+
+# Escrever o JSON mesclado em um novo arquivo
+with open('import.js', 'w') as output_file:
+    js_code =(
         f'a = {json.dumps(merged_json, indent=2)};\n\n'
         'window.$nuxt.$root.$children[2].$children[0].$children[0]._data.elements = a.elements;\n'
         'window.$nuxt.$root.$children[2].$children[0].$children[0]._data.discoveries = a.discoveries;\n'
-        'alert(`${a.elements.length} elements unlocked in total.`);'
+        'alert(`${a.elements.length} elements discovered!`);'
     )
+    output_file.write(js_code)
 
-    return js_code
-
-if __name__ == "__main__":
-    file1 = "json1.json"
-    file2 = "json2.json"
-
-    js_code = generate_js_code(file1, file2)
-
-    with open("import.js", 'w') as output:
-        output.write(js_code)
-
-    print("JavaScript code saved to import.js")
+print("Javascript code to paste on terminal saved in 'import.js'")
